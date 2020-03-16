0c267088b751691f4770f87cce97fe7326748020
Allison Bradley <allison.bradley@outlook.com>
2020-03-16 11:55:21 -0600

    Added conditional rendering for favorites list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Searchbar from './Searchbar'
 import History from './History'
 import { SidebarWrapper } from '../styles'
+import { DayContext } from '../utils/DayContext'
 
 const Sidebar = () => {
     
+const { btnArray } = useContext(DayContext)
+console.log(btnArray.length)
+
     return (
         <SidebarWrapper>
         <Searchbar />
         <hr/>
         <br/>
-        <h2>Check out your favorites below:</h2>
+        {btnArray.length !== 0 ? (<h2>Check out your favorites below:</h2>) : (<h2>Use the "Add to Favorites" button to start your favorites list!</h2>)}
         <History />
         </SidebarWrapper>
     )
