5659f659d66d6caa95cd9aea2e37f797fe8da3e6
Allison Bradley <allison.bradley@outlook.com>
2020-04-04 14:35:24 -0600

    Updates

---
+++ precache-manifest.55c1c00a7e6784294adef35ce5c118b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a23ba5d63b60c2a3ad59c03058a2f6",
+    "url": "/weather-buddy/index.html"
+  },
+  {
+    "revision": "38271387b2842b3be101",
+    "url": "/weather-buddy/static/css/main.21c20a07.chunk.css"
+  },
+  {
+    "revision": "bafee3dac94360f1b9d1",
+    "url": "/weather-buddy/static/js/2.d048d77b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weather-buddy/static/js/2.d048d77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38271387b2842b3be101",
+    "url": "/weather-buddy/static/js/main.21173181.chunk.js"
+  },
+  {
+    "revision": "b0a936d88df1e0ec066b",
+    "url": "/weather-buddy/static/js/runtime-main.55792872.js"
+  }
+]);
